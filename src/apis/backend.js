3d721f806309b57a96854bf6a6bcacdf8a9787ca
gsr <gshawnr@gmail.com>
2022-11-05 11:53:35 -0700

    add deployment notes

--- src/apis/backend.js
+++ src/apis/backend.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const backend = axios.create({
-  baseURL: "http://localhost:5000/api/",
+  baseURL: "http://localhost:5000/api/", // online: /api/  offline: http://localhost:5000/api/
   headers: {
     Authorization: `Bearer ${localStorage.getItem("investmentsToken")}`,
     "Content-Type": "application/json",
